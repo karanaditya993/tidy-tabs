67e75aa48a848f1982641b1c2a13623c89c128dd
Karan Aditya <karanaditya993@gmail.com>
2021-01-08 20:18:32 -0500

    feat: Add partitioned tabs

--- popup.js
+++ popup.js
@@ -1,22 +1,38 @@
 (()  => {
-  let tabsData
+  let tabsData = []
   const closeBtn = document.getElementById('close')
+  const GET_DOMAIN_REGEX = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img
 
-  const renderTabList = (tabs) => {
-    debugger
-    tabsData = tabs
-    const tabsEl = document.getElementsByClassName('tabs')[0]
+  const partitionTabs = (tabs) => {
     tabs.map((tab) => {
+      const matchedUrl = tab.url.match(GET_DOMAIN_REGEX)[0]
+      const sameTab = tabsData.find(t => t.url === matchedUrl)
+      if (sameTab) {
+        sameTab.tabIds.push(tab.id)
+      } else {
+        tabsData.push({
+          url: matchedUrl,
+          tabIds: [tab.id],
+        })
+      }
+    })
+    renderTabList(tabsData)
+  }
+
+  const renderTabList = (tabsData) => {
+    const tabsEl = document.getElementsByClassName('tabs')[0]
+    tabsData.map((tab, idx) => {
       const tabEl = document.createElement('div')
       tabEl.className = 'tab'
       const checkboxEl = document.createElement('input')
       checkboxEl.name = 'tab'
       checkboxEl.type = "checkbox"
-      checkboxEl.id = tab.index
-      checkboxEl.value = tab.id
+      checkboxEl.id = idx
+      checkboxEl.value = tab.url
+      checkboxEl.dataset.tabIds = tab.tabIds.join(',')
       const label = document.createElement('label')
       label.htmlFor = checkboxEl.id
-      label.innerText = tab.title
+      label.innerText = tab.url
       tabEl.appendChild(checkboxEl)
       tabEl.appendChild(label)
       tabsEl.appendChild(tabEl)
@@ -24,12 +40,16 @@
   }
 
   closeBtn.addEventListener('click', () => {
-    const checkedTabIds = [...document.querySelectorAll('input[name=tab]:checked')].map((box) => Number(box.value));
+    const checkedTabIds = [...document.querySelectorAll('input[name=tab]:checked')]
+                            .map(box => box.dataset.tabIds.split(',')
+                            .map(val => Number(val)))
+                            .flat()
     chrome.tabs.remove(checkedTabIds)
     window.location.reload()
   })
 
+
   window.onload = () => {
-    chrome.tabs.getAllInWindow(null, renderTabList)
+    chrome.tabs.query({}, partitionTabs)
   }
 })()
