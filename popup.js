2dbdb1fc7dc5b65c389c5b14045f386b93fe3682
Karan Aditya <karanaditya993@gmail.com>
2021-01-09 11:12:49 -0500

    chore: Refactor renderList fn

--- popup.js
+++ popup.js
@@ -24,6 +24,7 @@
         })
       }
     })
+    tabsData = tabsData.sort((tab1, tab2) => tab2.tabIds.length - tab1.tabIds.length)
     renderTabList(tabsData)
   }
 
@@ -56,101 +57,111 @@
     return color
   }
 
+  const isOnListView = () => (
+    document.getElementById('list-container').style.display !== 'none'
+    && document.getElementsByClassName('list-tabs')[0].children.length !== tabsData.length
+  )
+
+  const isOnGridView = () => (
+    document.getElementById('grid-container').style.display !== 'none'
+        && document.getElementsByClassName('grid-tabs')[0].children.length !== tabsData.length
+  )
+
   const renderTabList = () => {
-    tabsData.sort((tab1, tab2) => tab2.tabIds.length - tab1.tabIds.length)
     tabsData.map((tab, idx) => {
-      if (document.getElementById('list-container').style.display !== 'none'
-        && document.getElementsByClassName('list-tabs')[0].children.length !== tabsData.length
-      ) {
-        const tabsEl = document.getElementsByClassName('list-tabs')[0]
-        const badgeEl     = document.createElement('div')
+      let tabsEl
+      let badgeEl       = document.createElement('div')
+      let tabEl         = document.createElement('div')
+      tabEl.className   = 'tab'
+      badgeEl.className = 'tab-badge-icon'
+
+      if (isOnListView()) {
+        tabsEl            = document.getElementsByClassName('list-tabs')[0]
         const checkboxEl  = document.createElement('input')
         const favicon     = document.createElement('img')
         const label       = document.createElement('label')
-        const tabEl       = document.createElement('div')
 
-        tabEl.className   = 'tab'
         checkboxEl.name   = 'tab'
         checkboxEl.type   = 'checkbox'
         checkboxEl.id     = idx
         checkboxEl.value  = tab.url
         checkboxEl.dataset.tabIds = tab.tabIds.join(',')
+
         favicon.src       = tab.icon
+
         label.className   = 'tab-label'
         label.htmlFor     = checkboxEl.id
         label.innerHTML   = `<span class="tab-name">${tab.name}</span>`
-        badgeEl.className = 'tab-badge'
+
+        
         badgeEl.innerText = tab.tabIds.length
         badgeEl.style.backgroundColor = getBadgeColor(tab.tabIds.length)
+
         tabEl.appendChild(checkboxEl)
         tabEl.appendChild(favicon)
         tabEl.appendChild(label)
-        tabEl.appendChild(badgeEl)
-        tabsEl.appendChild(tabEl)
-      } else if (document.getElementById('grid-container').style.display !== 'none'
-        && document.getElementsByClassName('grid-tabs')[0].children.length !== tabsData.length
-      ) {
-        const tabsEl = document.getElementsByClassName('grid-tabs')[0]
-        const badgeEl     = document.createElement('div')
-        const numBadge    = document.createElement('div')
-        const closeIcon   = document.createElement('div')
-        const tabEl       = document.createElement('div')
+      } else if (isOnGridView()) {
+        tabsEl               = document.getElementsByClassName('grid-tabs')[0]
+        const numBadge       = document.createElement('div')
+        const closeIcon      = document.createElement('div')
 
-        tabEl.className   = 'tab'
         tabEl.dataset.tabIds = tab.tabIds.join(',')
-        closeIcon.className = 'close-icon'
-        badgeEl.className = 'tab-badge-icon'
+        
+        closeIcon.className  = 'close-icon'
+
+        numBadge.className   = 'num-badge'        
+        numBadge.innerText   = tab.tabIds.length
+        numBadge.style.backgroundColor = getBadgeColor(tab.tabIds.length)
+
         badgeEl.style.backgroundImage = `url(${tab.icon})`
         badgeEl.style.backgroundRepeat = 'no-repeat'
         badgeEl.style.backgroundSize = 'cover'
-        numBadge.className = 'num-badge'
-        numBadge.style.backgroundColor = getBadgeColor(tab.tabIds.length)
-        numBadge.innerText = tab.tabIds.length
+        
         badgeEl.appendChild(closeIcon)
         badgeEl.appendChild(numBadge)
-        tabEl.appendChild(badgeEl)
-        tabsEl.appendChild(tabEl)
       }
+      tabEl.appendChild(badgeEl)
+      tabsEl.appendChild(tabEl)
     })
   }
 
   const addListeners = () => {
-    document.body.addEventListener('click', (el) => {
-      if (el.target.id === 'switch-checkbox') {
-        if (el.target.checked) {
-          document.getElementById('grid-container').style.display = 'block'
-          document.getElementById('list-container').style.display = 'none'
-        } else {
-          document.getElementById('grid-container').style.display = 'none'
-          document.getElementById('list-container').style.display = 'block'
-        }
-        renderTabList()
-      }
+    // document.body.addEventListener('click', (el) => {
+    //   if (el.target.id === 'switch-checkbox' || el.target.className.includes('slider')) {
+    //     if (el.target.checked) {
+    //       document.getElementById('grid-container').style.display = 'block'
+    //       document.getElementById('list-container').style.display = 'none'
+    //     } else {
+    //       document.getElementById('grid-container').style.display = 'none'
+    //       document.getElementById('list-container').style.display = 'block'
+    //     }
+    //     return renderTabList()
+    //   }
 
-      if (document.getElementById('list-container').style.display !== 'none') {
-        if (el.target.className === 'tab') {
-          const input = el.target.querySelector('input')
-          input.checked = !input.checked
-          return
-        }
-        if (el.target.id === 'close') {
-          const checkedTabIds = [...document.querySelectorAll('input[name=tab]:checked')]
-                                .map(box => box.dataset.tabIds.split(',')
-                                .map(val => Number(val)))
-                                .flat()
-          chrome.tabs.remove(checkedTabIds, () => {
-            window.location.reload()
-          })
-          return
-        }
-      } else if (document.getElementById('grid-container').style.display !== 'none') {
-        const tabEl = el.target.closest('.tab')
-        const tabIds = tabEl.dataset.tabIds.split(',').map(val => Number(val))
-        chrome.tabs.remove(tabIds, () => {
-          window.location.reload()
-        })
-      }
-    })
+    //   if (document.getElementById('list-container').style.display !== 'none') {
+    //     if (el.target.className === 'tab') {
+    //       const input = el.target.querySelector('input')
+    //       input.checked = !input.checked
+    //       return
+    //     }
+    //     if (el.target.id === 'close') {
+    //       const checkedTabIds = [...document.querySelectorAll('input[name=tab]:checked')]
+    //                             .map(box => box.dataset.tabIds.split(',')
+    //                             .map(val => Number(val)))
+    //                             .flat()
+    //       chrome.tabs.remove(checkedTabIds, () => {
+    //         window.location.reload()
+    //       })
+    //       return
+    //     }
+    //   } else if (document.getElementById('grid-container').style.display !== 'none') {
+    //     const tabEl = el.target.closest('.tab')
+    //     const tabIds = tabEl.dataset.tabIds.split(',').map(val => Number(val))
+    //     chrome.tabs.remove(tabIds, () => {
+    //       window.location.reload()
+    //     })
+    //   }
+    // })
   }
 
   window.onload = () => {
