398d18ccbea35828c6c05b383e93c91b0c5d14d5
Karan Aditya <karanaditya993@gmail.com>
2021-01-15 20:57:26 -0500

    chore: Add typescript and appropriate linters

---
+++ tt-app/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default () => (
+  <div className="App">
+    <header className="App-header">
+      My Chrome Extension
+    </header>
+  </div>
+);
